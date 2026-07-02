b6a7159458c2b5c89314ea2e453a6fc9a8789386
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-02 19:07:02 +0200

    fix responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app-bar class="bg-primary pl-4 pr-4">
+  <v-app-bar class="bg-primary">
     <v-app-bar-title>
       <span style="cursor:pointer" role="link" tabindex="0" @click="$router.push('/')" @keydown.enter="$router.push('/')">
         <img src="/favicon.png" height="28" width="28" alt="Open Prices logo" style="vertical-align:bottom" />
         {{ constants.APP_NAME }}
       </span>
     </v-app-bar-title>
-    <v-btn v-if="!display.smAndUp" icon="mdi-plus-circle-outline" to="/events/create" aria-label="Ajouter" />
+    <v-btn v-if="!display.smAndUp.value" icon="mdi-plus-circle-outline" to="/events/create" aria-label="Ajouter" />
     <v-btn v-else prepend-icon="mdi-plus-circle-outline" to="/events/create" aria-label="Ajouter">
       Ajouter un événement
     </v-btn>
