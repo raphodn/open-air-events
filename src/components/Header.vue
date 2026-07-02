e4f50a7a2af1efc121d352e001dd584de3a32418
Raphaël Odini <raphodn@users.noreply.github.com>
2026-07-02 19:10:03 +0200

    feat(Header): ajout de l'icon à gauche. et d'un bouton direct vers le formulaire à droite (#23)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,23 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-app-bar-title style="cursor:pointer;" to="/" @click="$router.push('/')">CinéPleinAir</v-app-bar-title>
+  <v-app-bar class="bg-primary">
+    <v-app-bar-title>
+      <span style="cursor:pointer" role="link" tabindex="0" @click="$router.push('/')" @keydown.enter="$router.push('/')">
+        <img src="/favicon.png" height="28" width="28" alt="Open Prices logo" style="vertical-align:bottom" />
+        {{ constants.APP_NAME }}
+      </span>
+    </v-app-bar-title>
+    <v-btn v-if="!display.smAndUp.value" icon="mdi-plus-circle-outline" to="/events/create" aria-label="Ajouter" />
+    <v-btn v-else prepend-icon="mdi-plus-circle-outline" to="/events/create" aria-label="Ajouter">
+      Ajouter un événement
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script setup>
+import { useDisplay } from 'vuetify'
+import constants from '../constants'
+
+const display = useDisplay()
 </script>
 
 <style scoped>
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const OEDB_WHAT_DEFAULT = 'culture.cinema.outdoor'
 
 export default {
+  APP_NAME: 'CinéPleinAir',
   OEDB_WIKI_URL: 'https://wiki.openstreetmap.org/wiki/OpenEventDatabase',
   OEDB_API_URL: 'https://api.openeventdatabase.org',
   OEDB_WHAT_DEFAULT,
