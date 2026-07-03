cdf8a5d495759a5964a8dbdf685e37b57f2a627b
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-03 11:53:43 +0200

    feat(Footer): footer basique avec des stats et le lien vers Github

---
+++ src/components/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-footer class="flex-grow-0" color="grey">
+    <v-row no-gutters>
+      <v-col class="text-center">
+        <p>
+          {{ eventsCount }} événements.
+          <br v-if="!display.smAndUp.value" />
+          Dernière mise à jour {{ relativeLastSyncDate }}.
+        </p>
+        <v-btn class="mx-2 my-2" variant="text" prepend-icon="mdi-refresh" @click="refreshEvents">Rafraîchir</v-btn>
+        <v-btn class="mx-2 my-2" variant="text" prepend-icon="mdi-github" :href="constants.APP_GITHUB_URL" target="_blank">Github</v-btn>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup>
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useDisplay } from 'vuetify'
+import constants from '../constants'
+import dateUtils from '../utils/date.js'
+import { useEventsStore } from '../stores/events.js'
+
+const eventsStore = useEventsStore()
+const { eventsCount, eventsLastSyncDate } = storeToRefs(eventsStore)
+const display = useDisplay()
+const now = ref(Date.now())
+let intervalId = null
+
+const relativeLastSyncDate = computed(() => {
+  now.value
+  return eventsLastSyncDate.value
+    ? dateUtils.formatRelativeTime(eventsLastSyncDate.value)
+    : 'N/A'
+})
+
+const refreshEvents = () => {
+  eventsStore.fetchEvents({ forceRefresh: true })
+    .catch((error) => {
+      console.error('Error refreshing events:', error)
+    })
+}
+
+onMounted(() => {
+  // Update the "now" value every 10 seconds to keep the relative time display accurate
+  intervalId = window.setInterval(() => {
+    now.value = Date.now()
+  }, 1000 * 10)
+})
+
+onUnmounted(() => {
+  if (intervalId) {
+    window.clearInterval(intervalId)
+  }
+})
+</script>
+
+<style scoped>
+</style>
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ const OEDB_WHAT_DEFAULT = 'culture.cinema.outdoor'
 
 export default {
   APP_NAME: 'CinéPleinAir',
+  APP_GITHUB_URL: 'https://github.com/raphodn/open-air-events',
   OEDB_WIKI_URL: 'https://wiki.openstreetmap.org/wiki/OpenEventDatabase',
   OEDB_API_URL: 'https://api.openeventdatabase.org',
   OEDB_WHAT_DEFAULT,
