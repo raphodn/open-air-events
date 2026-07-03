db94c03bf737edad7df538ef5a90953c41a900c2
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-03 10:32:54 +0200

    preload all events on startup

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,20 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import Header from './components/Header.vue'
+import oedbService from './services/openeventdatabase.js'
+
+const preloadEvents = () => {
+  oedbService.getEvents()
+    .catch((error) => {
+      console.error('Error preloading events on app startup:', error)
+    })
+}
+
+onMounted(() => {
+  preloadEvents()
+})
 </script>
 
 <style scoped>
