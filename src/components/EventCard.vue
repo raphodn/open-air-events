148ebc1dcd9596b7e8fd5188febac07fe720e040
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-03 16:02:45 +0200

    each card should have the same height

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-card :to="{ name: 'event-details', params: { id: event.properties.id } }" link>
-    <v-card-title>{{ event.properties.label }}</v-card-title>
-    <v-card-text>
-      <p>📅 {{ dateUtils.formatDate(event.properties.start) }} ({{ dateUtils.formatTime(event.properties.start) }})</p>
+  <v-card class="h-100 d-flex flex-column" :to="{ name: 'event-details', params: { id: event.properties.id } }" link>
+    <v-card-title :title="event.properties.label">{{ event.properties.label }}</v-card-title>
+    <v-card-text class="flex-grow-1">
+      <p>📅 {{ dateUtils.formatDate(event.properties.start) }} à {{ dateUtils.formatTime(event.properties.start) }}</p>
       <p>📍 {{ oedbService.eventLocationFullName(event) }}</p>
       <p>🔗 <a :href="event.properties.url" target="_blank" @click.stop>plus d'info</a></p>
     </v-card-text>
     <v-divider></v-divider>
-    <v-card-text class="text-caption text-grey-darken-1">
+    <v-card-text class="text-caption text-grey-darken-1" style="max-height:50px;">
       Ajouté le {{ dateUtils.formatDate(event.properties.createdate) }}
     </v-card-text>
   </v-card>
