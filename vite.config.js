baf34de0961e69891040353bff43009d23360a26
Raphael Odini <raphodn@users.noreply.github.com>
2026-07-07 13:36:10 +0200

    fix(PWA): rajouter aussi le scope dans le manifest. ref #72

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         background_color: '#F6C24A',
         display: 'standalone',
         start_url: '/',
+        scope: '/',
         icons: [
           { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
